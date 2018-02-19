dde5c8f86096ba212246952d7a156176f9d4e6ed
basaktugceeskili <basaktugceeskili@gmail.com>
2018-02-19 12:47:52 +0100

    Task 4

--- Lab1_Photometric_Color/color_constancy/AWB.m
+++ Lab1_Photometric_Color/color_constancy/AWB.m
@@ -1,9 +1,15 @@
-function [] = AWB(image)
-
-A = imread('awb.jpg');
-figure
-imshow(A,'InitialMagnification',25)
-title('Original Image')
-
-
+function [output] = AWB(I)
+    [w,h,~]=size(I);
+    red      = sum(sum(I(:,:,1)))/(w*h);
+    green      = sum(sum(I(:,:,2)))/(w*h);
+    blue      = sum(sum(I(:,:,3)))/(w*h);
+    average        = mean([red green blue]);
+    Kr         = average/red;
+    Kg         = average/green;
+    Kb         = average/blue;
+    output(:,:,1) = Kr*double(I(:,:,1));
+    output(:,:,2) = Kg*double(I(:,:,2));
+    output(:,:,3) = Kb*double(I(:,:,3));
+    output = uint8(output);
+    
 end
--- Lab1_Photometric_Color/color_constancy/main.m
+++ Lab1_Photometric_Color/color_constancy/main.m
@@ -1,2 +1,15 @@
 A = imread('awb.jpg');
 
+output = AWB(A);
+
+
+figure
+subplot(1,2,1)
+imshow(A)
+title('Original Image ');
+
+
+subplot(1,2,2)
+imshow(output)
+title('Color Corrected Image ');
+
